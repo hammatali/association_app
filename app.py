e28c23d0cd1da407d116ec0ba0cc7df5231573b6
hammatali <hammatalil86@gmail.com>
2026-04-09 11:13:33 +0300

    app modf

--- app.py
+++ app.py
@@ -168,4 +168,8 @@ def init_db():
 if __name__ == "__main__":
     init_db()
     port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    #app.run(host='0.0.0.0', port=port)
+    #app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
